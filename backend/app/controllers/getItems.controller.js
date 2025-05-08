2d6efb54a696f0869f6a19ab43634faf79782e8a
josefrias <josefrias@bmd-software.com>
2025-05-08 11:55:13 +0100

    Adapt backend of file manager to include offset and limit of fetched results (for lazy loading at folder level)

--- backend/app/controllers/getItems.controller.js
+++ backend/app/controllers/getItems.controller.js
@@ -4,67 +4,56 @@ const path = require("path");
 const BASE_PATH = require('./filesystem.controller');
 const normalizePath = require("./utils.controller");
 
-async function readDirRecursively(dirPath, workspace) {
-  let result = [];
-
+async function readDir(dirPath, workspace, offset, limit, state = { count: 0, collected: [] }) {
   const files = await fs.readdir(BASE_PATH + dirPath, { withFileTypes: true });
+  const total = files.length;
 
   for (const file of files) {
+    if (state.collected.length >= limit) break;
+
     const fullPath = normalizePath(path.join(BASE_PATH + dirPath, file.name));
     const relativePath = fullPath.replace(BASE_PATH, "");
-    // Need this so that UI can construct the tree, otherwise it does not show nothing with the react component
     const pathWithoutWorkSpace = relativePath.replace(workspace, "");
-    if (file.isDirectory()) {
-      // If the file is a directory, recurse into it and add information about each directory
-      // Ignore files that give errors, e.g. broken symlinks
-      let stats;
-      try {
-        stats = await fs.stat(fullPath);
-      } catch (error) {
-        continue;
-      }
-      result.push({
-        _id: relativePath,
-        name: file.name,
-        path: pathWithoutWorkSpace,
-        isDirectory: true,
-        createdAt: stats.birthtime,
-        updatedAt: stats.mtime,
-      });
+
+    let stats;
+    try {
+      stats = await fs.stat(fullPath);
+    } catch (error) {
+      console.log("error on file ", pathWithoutWorkSpace);
+      continue;
     }
-    else {
-      // Otherwise, just push the file's details
-      // Ignore files that give errors, e.g. broken symlinks
-      let stats;
-      try {
-        stats = await fs.stat(fullPath);
-      } catch (error) {
-        continue;
-      }
-      result.push({
-        _id: relativePath,
-        name: file.name,
-        path: pathWithoutWorkSpace,
-        isDirectory: false,
+
+    const item = {
+      _id: relativePath,
+      name: file.name,
+      path: pathWithoutWorkSpace,
+      isDirectory: file.isDirectory(),
+      createdAt: stats.birthtime,
+      updatedAt: stats.mtime,
+      ...(file.isDirectory() ? {} : {
         size: stats.size,
         mimeType: mime.lookup(fullPath),
-        createdAt: stats.birthtime,
-        updatedAt: stats.mtime,
-      });
+      }),
+    };
+
+    if (state.count >= offset) {
+      state.collected.push(item);
     }
+
+    state.count++;
   }
 
-  return result;
+  return {files: state.collected, total}
 }
 
 const getItems = async (req, res) => {
   try {
     directoryPath = "";
-    const { workspace, currentPath } = req.query;
+    const { workspace, additionalPath, offset, limit } = req.query;
     
-    addPath = currentPath || "";
+    addPath = additionalPath || "";
 
-    const files = await readDirRecursively(directoryPath + "/" + workspace + addPath, workspace);
+    const files = await readDir(directoryPath + "/" + workspace + addPath, workspace, offset, limit);
 
     res.status(200).json(files);
   } catch (error) {
