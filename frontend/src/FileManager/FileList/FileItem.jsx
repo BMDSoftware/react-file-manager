73b41dc0621d421656814bb598b5efcbd611e3b3
Martinho Tavares <martinho.tavares@bmd-software.com>
2025-08-19 12:21:37 +0100

    Add padded label to file list/grid checkboxes

--- frontend/src/FileManager/FileList/FileItem.jsx
+++ frontend/src/FileManager/FileList/FileItem.jsx
@@ -212,6 +212,7 @@ const FileItem = ({
             id={file.name}
             checked={fileSelected}
             className={`selection-checkbox ${checkboxClassName}`}
+            labelClassName={`selection-checkbox-label`}
             onChange={handleCheckboxChange}
             onClick={(e) => e.stopPropagation()}
           />
--- frontend/src/components/Checkbox/Checkbox.jsx
+++ frontend/src/components/Checkbox/Checkbox.jsx
@@ -1,18 +1,19 @@
 import "./Checkbox.scss";
 
-const Checkbox = ({ name, id, checked, onClick, onChange, className = "", title, disabled = false }) => {
+const Checkbox = ({ name, id, checked, onClick, onChange, className = "", labelClassName = "", title, disabled = false }) => {
   return (
-    <input
-      className={`fm-checkbox ${className}`}
-      type="checkbox"
-      name={name}
-      id={id}
-      checked={checked}
-      onClick={onClick}
-      onChange={onChange}
-      title={title}
-      disabled={disabled}
-    />
+    <label htmlFor={id} aria-label={`${name} checkbox`} onClick={onClick} className={labelClassName}>
+      <input
+        className={`fm-checkbox ${className}`}
+        type="checkbox"
+        name={name}
+        id={id}
+        checked={checked}
+        onChange={onChange}
+        title={title}
+        disabled={disabled}
+      />
+    </label>
   );
 };
 
